483ea1a732644b131fc931aeee5c3e879eba7c99
MZuHaYer AHQ Heemel <128010333+MrZ3RO-K3NiS7@users.noreply.github.com>
2023-03-24 03:00:09 +0600

    CFX

---
+++ CODEFORCES/493D (Vasya and Chess).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    if (N % 2 != 0)
+    {
+        cout << "black";
+    }
+    else
+    {
+        cout << "white" << endl;
+        cout << "1 2";
+    }
+    return 0;
+}
