7c79608d857069afa86fa5d1df75bd29ce739812
MZuHaYer AHQ Heemel <128010333+MrZ3RO-K3NiS7@users.noreply.github.com>
2023-04-03 02:55:04 +0600

    CFX

---
+++ CODEFORCES/1812B (Was it Rated?).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    if (N == 15 || N == 20 || N == 21)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        cout << "YES";
+    }
+    return 0;
+}
