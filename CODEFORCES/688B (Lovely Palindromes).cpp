2d62394f0bc9d3432dd04350867bc999dba7477a
MZuHaYer AHQ Heemel <128010333+MrZ3RO-K3NiS7@users.noreply.github.com>
2023-04-18 16:33:44 +0600

    CFX

---
+++ CODEFORCES/688B (Lovely Palindromes).cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Nice_To_Meet_U int main()
+#define str string
+#define See_U_Again___BYE return 0;
+
+Nice_To_Meet_U
+{
+    str N, M, O;
+    cin >> N;
+    M=N;
+    reverse(N.begin(), N.end());
+    O=M+N;
+    cout << O << endl;
+    See_U_Again___BYE
+}
