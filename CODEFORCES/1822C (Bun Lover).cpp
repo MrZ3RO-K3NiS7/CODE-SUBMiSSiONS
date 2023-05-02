be35b74a74b16d3811ce031f24d18c8f7e0ef29e
MZuHaYer AHQ Heemel <128010333+MrZ3RO-K3NiS7@users.noreply.github.com>
2023-05-02 12:50:40 +0600

    CFX

---
+++ CODEFORCES/1822C (Bun Lover).cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Nice_To_Meet_U int main()
+#define ll long long int
+#define See_U_Again___BYE return 0;
+
+void solve()
+{
+    ll N;
+    cin >> N;
+    cout << (N * (N + 2)) + 2;
+}
+
+Nice_To_Meet_U
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+        cout << endl;
+    }
+    See_U_Again___BYE
+}
