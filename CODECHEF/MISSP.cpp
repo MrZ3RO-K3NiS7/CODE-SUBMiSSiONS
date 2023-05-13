cdc402ee6f058829eca0396f3eb0bc8271d1cb6c
MZuHaYer AHQ Heemel <128010333+MrZ3RO-K3NiS7@users.noreply.github.com>
2023-05-13 23:29:56 +0600

    CCX

--- CODECHEF/MISSP.cpp
+++ CODECHEF/MISSP.cpp
@@ -1,22 +1,36 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+#define Nice_To_Meet_U int main()
+#define ll long long int
+#define See_U_Again___BYE return 0;
 
-int main()
+void solve()
 {
-    int T;
-    cin >> T;
-    while (T--)
+    ll N, X;
+    cin >> N;
+    int arr[111111] = {0};
+    for (int i = 0; i < N; i++)
     {
-        int N;
-        cin >> N;
-        int X = 0;
-        for (int i = 0; i < N; i++)
-        {
-            int T;
-            cin >> T;
-            X = X ^ T;
-        }
-        cout << X << endl;
+        cin >> X;
+        arr[X]++;
     }
-    return 0;
+    for (int i = 0; i < 111111; i++)
+    {
+        if (arr[i] % 2 != 0)
+        {
+            cout << i << endl;
+            break;
+        }
+    }
+}
+
+Nice_To_Meet_U
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    See_U_Again___BYE
 }
