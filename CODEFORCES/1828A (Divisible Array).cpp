c5ee775828fc5d8823ec73f93df5d9dc911224b2
MZuHaYer AHQ Heemel <128010333+MrZ3RO-K3NiS7@users.noreply.github.com>
2023-05-15 01:58:35 +0600

    CFX

---
+++ CODEFORCES/1828A (Divisible Array).cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Nice_To_Meet_U int main()
+#define ll long long int
+#define See_U_Again___BYE return 0;
+
+void solve()
+{
+    ll N;
+    cin >> N;
+    for (int i = 1; i <= N; i++)
+    {
+        cout << i * 2 << " ";
+    }
+    cout << endl;
+}
+
+Nice_To_Meet_U
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    See_U_Again___BYE
+}
