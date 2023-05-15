0152d6edc7a38d75a6d12228bc1b084ec3c5fdd4
MZuHaYer AHQ Heemel <128010333+MrZ3RO-K3NiS7@users.noreply.github.com>
2023-05-16 03:35:53 +0600

    LCX

---
+++ LEETCODE/contains-duplicate.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int X = nums.size();
+        int Y = set<int>(nums.begin(), nums.end()).size();
+        if (X==Y)
+        {
+            return false;
+        }
+        return true;
+    }
+};
