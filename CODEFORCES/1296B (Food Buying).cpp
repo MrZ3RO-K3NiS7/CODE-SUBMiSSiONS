14b7722f77f5fed5554d3c7b7280a1cecbf96030
MZuHaYer AHQ Heemel <128010333+MrZ3RO-K3NiS7@users.noreply.github.com>
2023-05-16 22:15:46 +0600

    CFX

---
+++ CODEFORCES/1296B (Food Buying).cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Nice_To_Meet_U int main()
+#define ll long long int
+#define See_U_Again___BYE return 0;
+
+void solve()
+{
+    ll N, COUNT;
+    cin >> N;
+
+    COUNT = N + (N - 1) / 9;
+
+    cout << COUNT << endl;
+}
+
+Nice_To_Meet_U
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    See_U_Again___BYE
+}
