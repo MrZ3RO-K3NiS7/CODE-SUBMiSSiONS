e9240b69fc464cfd32119bd4cea0b2c30af9ed6e
MZuHaYer AHQ Heemel <128010333+MrZ3RO-K3NiS7@users.noreply.github.com>
2023-05-25 23:42:38 +0600

    CFX

---
+++ CODEFORCES/1837A (Grasshopper on a Line).cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define Nice_To_Meet_U          int main()
+#define MRZ                     ios_base
+#define ll                      long long
+#define db                      double
+#define ld                      long double
+#define ull                     unsigned long long
+#define str                     string
+#define vc                      vector
+#define pr                      pair
+#define mp                      map
+#define st                      set
+#define stk                     stack
+#define qu                      queue
+#define dq                      deque
+#define pb                      push_back
+#define pf                      push_front
+#define MIN                     INT_MIN
+#define MAX                     INT_MAX
+#define sz                      size()
+#define C(X)                    cout << X << endl
+#define all(exe)                exe.begin(), exe.end()
+#define f(exe)                  for (int i = 0; i < exe; i++)
+#define SOLN___XD               solution();
+#define Never_Give_Up           ll t;cin >> t;
+#define Trust_iN_Yourself       while (t--){solution();}
+#define See_You_Again___BYEEE   return 0;
+
+void sync()
+{
+    MRZ::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
+}
+
+void solution()
+{
+    ll X, K;
+    cin >> X >> K;
+    if (X % K == 0)
+    {
+        C(2 << endl
+            << 1 << " " << X - 1);
+    }
+    else
+    {
+        C(1 << endl
+            << X);
+    }
+}
+
+Nice_To_Meet_U
+{
+    sync();
+
+    Never_Give_Up
+
+        Trust_iN_Yourself
+
+            See_You_Again___BYEEE
+}
