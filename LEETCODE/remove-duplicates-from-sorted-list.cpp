b760218d5972177b9533d3c718c285886fd7f8e5
MZuHaYer AHQ Heemel <128010333+MrZ3RO-K3NiS7@users.noreply.github.com>
2023-06-28 01:55:42 +0600

    LCX

---
+++ LEETCODE/remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head)
+    {
+        ListNode *tmp = head;
+        if (tmp == NULL)
+        {
+            return head;
+        }
+        while (tmp->next != NULL)
+        {
+            if (tmp->val == tmp->next->val)
+            {
+                tmp->next = tmp->next->next;
+            }
+            else
+            {
+                tmp = tmp->next;
+            }
+        }
+        return head;
+    }
+};
