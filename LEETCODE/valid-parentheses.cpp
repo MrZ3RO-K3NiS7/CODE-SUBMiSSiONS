7ed8d71600fbf7ebf20e3a7687419ee767fe52ce
MZuHaYer AHQ Heemel <128010333+MrZ3RO-K3NiS7@users.noreply.github.com>
2023-06-28 23:36:25 +0600

    LCX

---
+++ LEETCODE/valid-parentheses.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isValid(string s)
+    {
+        stack<char> st;
+        int N = s.size();
+        for (int i = 0; i < N; i++)
+        {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+            {
+                st.push(s[i]);
+            }
+            else
+            {
+                if (st.empty())
+                {
+                    return false;
+                }
+                if ((s[i] == ')' && st.top() == '(') ||
+                    (s[i] == '}' && st.top() == '{') ||
+                    (s[i] == ']' && st.top() == '['))
+                {
+                    st.pop();
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        if (st.empty())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
