9a9847232ec3f8be849b0937acbd1aec776ba301
MZuHaYer AHQ Heemel <128010333+MrZ3RO-K3NiS7@users.noreply.github.com>
2023-07-04 23:26:02 +0600

    CNX

--- CODiNG-NiNJAS/insert-an-element-at-its-bottom-in-a-given-stack_1171166.cpp
+++ CODiNG-NiNJAS/insert-an-element-at-its-bottom-in-a-given-stack_1171166.cpp
@@ -1,3 +1,5 @@
+// RECURSiON !!!
+
 #include <bits/stdc++.h>
 void solve(stack<int> &myStack, int x)
 {
@@ -14,5 +16,27 @@ void solve(stack<int> &myStack, int x)
 stack<int> pushAtBottom(stack<int> &myStack, int x)
 {
     solve(myStack, x);
+    return myStack;
+}
+
+
+
+// NO NEED RECURSiON !!!
+
+#include <bits/stdc++.h>
+stack<int> pushAtBottom(stack<int> &myStack, int x)
+{
+    stack<int> newSt;
+    while (!myStack.empty())
+    {
+        newSt.push(myStack.top());
+        myStack.pop();
+    }
+    newSt.push(x);
+    while (!newSt.empty())
+    {
+        myStack.push(newSt.top());
+        newSt.pop();
+    }
     return myStack;
 }
